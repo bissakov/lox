7aa710cedaed98f97f1ec0db54b6e19c45185b29
Alikhan Bissakov <78418192+bissakov@users.noreply.github.com>
2024-07-18 23:22:10 +0500

    (chore): Lox project initialization

---
+++ build.bat
@@ -0,0 +1,9 @@
+@echo off
+
+call vcvarsall.bat x64 > nul 2>&1
+mkdir build > nul 2>&1
+pushd build
+
+cl -Fe: lox.exe -nologo -Oi -GR- -EHa- -MT -Gm- -Od -W4 -WX -wd4201 -wd4127 -wd4100 -FC -Z7 -Fm: lox.map ../src/main.cpp /link -opt:ref
+
+popd
